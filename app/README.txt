6837eb435bd9ff6c31bcc48d99fa5af78b2c1689
Lorenzo Lando <llando@google.com>
2019-05-17 18:56:37 +0100

    searchbar basic programming

---
+++ app/README.txt
@@ -0,0 +1,14 @@
+UI
+
+aggiungi una funzionalita` random recipes
+
+API
+
+primo esempio trovato per food api a pagamento:
+https://rapidapi.com/spoonacular/api/recipe-food-nutrition/pricing
+
+BACKEND
+
+
+
+
---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+//retrive the info from the search box
+let searchBox = document.querySelector('#main-search');
+let ingredient;
+searchBox.addEventListener('keypress', function(event) {
+    if(event.key == "Enter"){
+         ingredient = [];
+         ingredient  = searchBox.value.split(' ');
+         console.log(ingredient);
+    }
+});
+
+
